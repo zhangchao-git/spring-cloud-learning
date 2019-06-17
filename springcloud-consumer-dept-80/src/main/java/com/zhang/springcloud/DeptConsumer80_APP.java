c4527f7e2b6f89b726ff5fb3f51d2a3609fdb680
zhangchao-git <1045685272@qq.com>
2019-06-17 11:28:22 +0800

    consumer

--- springcloud-consumer-dept-80/src/main/java/com/zhang/springcloud/DeptConsumer80_APP.java
+++ springcloud-consumer-dept-80/src/main/java/com/zhang/springcloud/DeptConsumer80_APP.java
@@ -1,11 +1,13 @@
 package com.zhang.springcloud;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @Auther: zhangchao
  * @Date: 2019/6/17 11:20
  */
+@SpringBootApplication
 public class DeptConsumer80_APP {
 
     public static void main(String[] args) {
