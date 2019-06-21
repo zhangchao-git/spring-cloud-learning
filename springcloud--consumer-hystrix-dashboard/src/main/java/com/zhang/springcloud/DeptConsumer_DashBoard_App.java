3667f57d41179fff782d5a8f46487dc97e459828
zhangchao-git <1045685272@qq.com>
2019-06-21 11:40:35 +0800

    hystrix-dashboard

---
+++ springcloud--consumer-hystrix-dashboard/src/main/java/com/zhang/springcloud/DeptConsumer_DashBoard_App.java
@@ -0,0 +1,19 @@
+package com.zhang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @Auther: zhangchao
+ * @Date: 2019/6/17 11:20
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class DeptConsumer_DashBoard_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer_DashBoard_App.class, args);
+    }
+
+}
