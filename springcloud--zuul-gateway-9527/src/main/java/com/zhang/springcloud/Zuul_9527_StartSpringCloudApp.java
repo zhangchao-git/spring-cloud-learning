a3082cf5c06432d16f05a0b131fb93d542dffa6c
zhangchao-git <1045685272@qq.com>
2019-06-21 15:32:10 +0800

    zuul

---
+++ springcloud--zuul-gateway-9527/src/main/java/com/zhang/springcloud/Zuul_9527_StartSpringCloudApp.java
@@ -0,0 +1,20 @@
+package com.zhang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @Auther: zhangchao
+ * @Date: 2019/6/21 14:46
+ */
+
+@SpringBootApplication
+@EnableZuulProxy
+public class Zuul_9527_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(Zuul_9527_StartSpringCloudApp.class, args);
+    }
+}
+
+
